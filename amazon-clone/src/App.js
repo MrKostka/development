583923f016b5b69beb11836bfa388e4c813e2121
MrKostka <markokostka@zoznam.sk>
2021-07-18 20:48:19 +0200

    Home page amazon-clone completed

--- amazon-clone/src/App.js
+++ amazon-clone/src/App.js
@@ -3,13 +3,26 @@ import './App.css';
 import Header from './Header';
 import Home from './Home';
 
+import { BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+
 function App() {
   return (
     //BEM name convention - app, no App
-    <div className="app">
-      <Header />
-      <Home />
-    </div>
+    <Router>
+      <div className="app">
+        <Switch>
+          <Route path="/checkout">
+            <Header />
+            <h1>I AM CHECKOUT PAGE</h1>
+          </Route> 
+          <Route path="/">
+            <Header />
+            <Home />
+          </Route>                       
+        </Switch>       
+      </div>
+    </Router>
+    
   );
 }
 
--- amazon-clone/src/Home.js
+++ amazon-clone/src/Home.js
@@ -12,22 +12,52 @@ function Home() {
                 />
                 <div className="home__row">
                     <Product 
-                        title='The Lean Startup' 
+                        id="25488157"
+                        title='The Lean Startup: How COnstant Innovation Creates Radically Successful Businesses Paperback' 
                         price={11.96}
                         image="https://images-na.ssl-images-amazon.com/images/I/51T-sMqSMiL._SX329_BO1,204,203,200_.jpg"
                         rating={5}
                     />
-                    <Product />
+                    <Product 
+                        id="14683495"
+                        title='Dell Ultrasharp U2719DX 27-Inch WQHD 2560x1440 Resolution IPS Monitor with Infinity Edge Bezels, Black ' 
+                        price={344.99}
+                        image="https://images-na.ssl-images-amazon.com/images/I/71YqpUwrpBL._AC_SL1500_.jpg"
+                        rating={4}
+                    />
+                </div>
+                <div className="home__row">
+                    <Product 
+                        id="90103701"
+                        title="The History of Middle Earth, Volumes 1-12, and The Silmarillion" 
+                        price={9500.00}
+                        image="https://images-na.ssl-images-amazon.com/images/I/51jOKIg9EkL.jpg"
+                        rating={5}
+                    />
+                    <Product 
+                        id="56176329"
+                        title="Clara 92.5 Sterling Silver White Gold Plated Heart Solitaire Pendant Chain Necklace for Women & Girls " 
+                        price={1499.00}
+                        image="https://images-na.ssl-images-amazon.com/images/I/516qD0%2Bt16L._UL1100_.jpg"
+                        rating={4}
+                    />
+                    <Product 
+                        id="52009408"
+                        title="JM Speakers Bluetooth Desktop Computer Speakers Notebooks Home Audio Subwoofers (Color : White) " 
+                        price={177.72}
+                        image="https://images-na.ssl-images-amazon.com/images/I/5159%2Bv1oedL._AC_SL1024_.jpg"
+                        rating={0}
+                    />
                 </div>
 
                 <div className="home__row">
-                    <Product />
-                    <Product />
-                    <Product />
-                </div>
-
-                <div className="home__row">
-                    <Product />
+                    <Product 
+                        id="88267948"
+                        title="Luxsis Women's Slim Fit Jeans (Pack of 2) " 
+                        price={495.00}
+                        image="https://images-na.ssl-images-amazon.com/images/I/81-VKWQ196L._UL1500_.jpg"
+                        rating={4}
+                    />
                 </div>
 
 
