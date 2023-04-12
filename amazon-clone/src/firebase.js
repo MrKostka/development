8ddd83bc53455e7139b93a99c7471aafd4af5814
marek.kostka <marek.kostka@ricb.ch>
2023-04-12 22:53:49 +0200

    merge

---
+++ amazon-clone/src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDWFcEdPCLIcdHXcSn3_crKGAoZaA3Zvlg",
+  authDomain: "clone-d15fc.firebaseapp.com",
+  projectId: "clone-d15fc",
+  storageBucket: "clone-d15fc.appspot.com",
+  messagingSenderId: "38619835221",
+  appId: "1:38619835221:web:e70935414c4aac3f159ddc",
+  measurementId: "G-X9E40CD73Z"
+};
